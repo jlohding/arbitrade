fab4ac1f5d40155309f00d91302e9f83b44f6cf5
Jerry <81377042+jlohding@users.noreply.github.com>
2021-12-17 02:28:25 +0800

    feat: crontab sync with market open

---
+++ src/arbitrade/helpers/cron_update.py
@@ -0,0 +1,78 @@
+import datetime as dt
+import zoneinfo
+import configs
+
+class CronUpdate:
+    def __init__(self, app, strategy_builder):
+        self.app = app
+        self.strategy_builder = strategy_builder
+        self.constants = configs.Constants()
+        self.cron_path = configs.get_cron_path()
+
+    def __get_contract_details(self):
+        ib_contracts = self.strategy_builder.get_base_ib_contracts()
+        contract_details = [(self.constants.ib_symbol_to_symbol(contract.symbol), 
+                             contract.secType, 
+                             self.app.get_contract_details(contract)) 
+                             for contract in ib_contracts.values()]
+        return contract_details
+    
+    def __get_tz_offset(self, timezone):
+        delta = dt.datetime.now(zoneinfo.ZoneInfo(timezone)).utcoffset()
+        return delta
+
+    def __parse_utc_hours(self, trading_hours, tz_offset):
+        open_hours = []
+        sessions = trading_hours.split(";")
+        for session in sessions:
+            if "CLOSED" in session:
+                continue
+            else:
+                start, end = session.split("-")
+                utc_start, utc_end = [dt.datetime.strptime(tm, "%Y%m%d:%H%M") - tz_offset for tm in (start, end)]
+                aware_utc_start, aware_utc_end = [t.replace(tzinfo=zoneinfo.ZoneInfo("UTC")) for t in (utc_start, utc_end)]
+                open_hours.append((aware_utc_start, aware_utc_end))
+        return open_hours
+
+    def __get_contract_open_hours(self):
+        contract_details = self.__get_contract_details()
+        utc_open_hours_d = {(symbol, kind): (self.__parse_utc_hours(det.tradingHours, self.__get_tz_offset(det.timeZoneId))) for symbol, kind, det in contract_details}
+        return utc_open_hours_d
+
+    def __get_next_session_open(self):
+        utc_open_hours_d = self.__get_contract_open_hours()
+        utcnow = dt.datetime.now(zoneinfo.ZoneInfo("UTC"))
+        next_session_d = {}
+
+        for asset, sessions in utc_open_hours_d.items():
+            for start, end in sessions:
+                if utcnow < start:
+                    next_session_d[asset] = start
+                    break
+            else:
+                raise Exception(f"No next open found for {asset}")
+        return next_session_d
+    
+    def __get_cron_d(self):
+        next_session_d = self.__get_next_session_open()
+        cron_d = {}
+        for asset, dt in next_session_d.items():
+            cron = f"{dt.minute} {dt.hour} {dt.day} {dt.month} *"
+            dt_string = dt.strftime("%Y%m%d %H:%M:%S")
+            if (cron, dt_string) not in cron_d:
+                cron_d[(cron, dt_string)] = []
+            cron_d[(cron, dt_string)].append(asset)
+        return cron_d
+    
+    def update_crontab(self):
+        cron_d = self.__get_cron_d()
+        commands = []
+        for key, assets in cron_d.items():
+            cron, dt_string = key
+            asset_string = " ".join([",".join(asset) for asset in assets])
+            command = f"{cron} python __main__.py -t '{asset_string}' -d '{dt_string}'\n"
+            commands.append(command)
+        
+        with open(self.cron_path, "w") as file:
+            for command in commands:
+                file.write(command)
